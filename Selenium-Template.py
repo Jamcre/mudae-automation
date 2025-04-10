50ab09a57cf351b7418c10f6cb7269df20304b72
jamcre <jmcrespo123@gmail.com>
2025-04-10 18:13:22 -0400

    changed from element finding

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -47,14 +47,14 @@ driver.get("https://discord.com/login")
 
 
 time.sleep(3)
-email_field = driver.find_element(By.XPATH, '//*[@id="uid_32"]')
+email_field = driver.find_element(by=By.NAME, value='email')
 print(email_field)
-pass_field = driver.find_element(By.XPATH, '//*[@id="uid_34"]')
+pass_field = driver.find_element(by=By.NAME, value='password')
 print(pass_field)
 time.sleep(2)
 email_field.send_keys(DISCORD_EMAIL)
 pass_field.send_keys(DISCORD_PASS)
-submit_button = driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/button[2]')
+submit_button = driver.find_element(by=By.XPATH, value='//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div/div/div/form/div[2]/div/div[1]/div[2]/button[2]')
 submit_button.send_keys(Keys.ENTER)
 time.sleep(3)
 
